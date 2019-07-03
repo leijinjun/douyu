55043a41046c044b3961d184d465c1467af29cd2
leijinjun <leijinjun@panpay.com.cn>
2019-07-03 19:05:33 +0800

    commit

--- douyu-parent/web-project/douyu-admin/src/main/java/com/lei2j/douyu/admin/web/config/WebConfigurer.java
+++ douyu-parent/web-project/douyu-admin/src/main/java/com/lei2j/douyu/admin/web/config/WebConfigurer.java
@@ -13,7 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfigurer implements WebMvcConfigurer {
 
-    @Value("#{'${cors.doamin.list}'.split(',')}")
+    @Value("#{'${cors.domain.list}'.split(',')}")
     private String[] origins;
 
     @Autowired
