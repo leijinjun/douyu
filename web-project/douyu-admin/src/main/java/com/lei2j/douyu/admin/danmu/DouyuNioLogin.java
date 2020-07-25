6117aeae4dac0fbaa296856e5474aadf7730816a
leijinjun <986111634@qq.com>
2020-07-25 17:02:25 +0800

    增加木兰许可证

--- web-project/douyu-admin/src/main/java/com/lei2j/douyu/admin/danmu/DouyuNioLogin.java
+++ web-project/douyu-admin/src/main/java/com/lei2j/douyu/admin/danmu/DouyuNioLogin.java
@@ -1,3 +1,15 @@
+/*
+* Copyright (c) [2020] [jinjun lei]
+* [douyu danmu] is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+* See the Mulan PSL v2 for more details.
+*/
+
 package com.lei2j.douyu.admin.danmu;
 
 import com.lei2j.douyu.admin.danmu.config.DouyuMessageConfig;
